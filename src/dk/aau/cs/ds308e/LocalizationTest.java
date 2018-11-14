43f9b734db76a20089d1dd68974ae9bc9ac7dce3
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-14 15:12:25 +0100

    localization test

---
+++ src/dk/aau/cs/ds308e/LocalizationTest.java
@@ -0,0 +1,13 @@
+package dk.aau.cs.ds308e;
+
+import java.util.Locale;
+
+public class LocalizationTest {
+
+    public static void main(String[] args) {
+        Locale enLocale = new Locale("en","US");
+        Locale daLocale = new Locale("da","DK");
+
+        System.out.println("test");
+    }
+}
