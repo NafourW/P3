87c0b1d571e85271b2cbf57f1799c7aeaebd762c
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-15 12:06:01 +0100

    scenebuilder

--- src/dk/aau/cs/ds308e/Main.java
+++ src/dk/aau/cs/ds308e/Main.java
@@ -1,9 +1,22 @@
 package dk.aau.cs.ds308e;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
-        Application.launch(GUI.class);
+        launch(args);
     }
 }
