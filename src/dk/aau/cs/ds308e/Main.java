ecf8e20657fcad6bf19694792f99e32b393604ce
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-15 15:02:04 +0100

    gui med internationale strings

--- src/dk/aau/cs/ds308e/Main.java
+++ src/dk/aau/cs/ds308e/Main.java
@@ -6,13 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Locale locale = new Locale("en","US");
+//      Locale locale = new Locale("da","DA");
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("gui.fxml"));
+        loader.setResources(ResourceBundle.getBundle("InternationalizedStrings", locale));
+
+        Parent root = loader.load();// FXMLLoader.load(getClass().getResource("gui.fxml"));
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
 
