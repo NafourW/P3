df8c72cd27f95d430b6e987d94e514cef79d89fb
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-19 11:06:50 +0100

    fix settings controller

--- src/dk/aau/cs/ds308e/gui/controllers/SettingsController.java
+++ src/dk/aau/cs/ds308e/gui/controllers/SettingsController.java
@@ -1,4 +1,12 @@
 package dk.aau.cs.ds308e.gui.controllers;
 
+import dk.aau.cs.ds308e.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SettingsController {
+    @FXML
+    private void backButtonAction(ActionEvent event) throws Exception{
+        Main.gui.changeView("MainMenu");
+    }
 }
