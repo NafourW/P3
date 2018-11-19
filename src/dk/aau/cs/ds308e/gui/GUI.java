9b02916a2e9dcaefee1ac2ebba4116769734221c
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-19 13:03:50 +0100

    sprog kan skiftes i settings

--- src/dk/aau/cs/ds308e/gui/GUI.java
+++ src/dk/aau/cs/ds308e/gui/GUI.java
@@ -9,15 +9,15 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class GUI {
-
     private Locale currentLocale;
     private ResourceBundle localStrings;
 
     private Stage window;
+    private String currentView;
 
     public GUI(){
         //TODO: load previously selected language
-        setLanguage("en", "US");
+        setLanguage(new Locale("en","US"));
     }
 
     //Set the main window
@@ -26,8 +26,8 @@ public class GUI {
     }
 
     //Set the application language
-    public void setLanguage(String language, String country) {
-        currentLocale = new Locale(language,country);
+    public void setLanguage(Locale language) {
+        currentLocale = language;
         localStrings = ResourceBundle.getBundle("InternationalizedStrings", currentLocale);
     }
 
@@ -38,10 +38,16 @@ public class GUI {
 
     //Change the current application view by loading FXML and replacing the current scene
     public void changeView(String view) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/views/" + view + ".fxml"), localStrings);
+        currentView = view;
+        Parent root = FXMLLoader.load(getClass().getResource("/views/" + currentView + ".fxml"), localStrings);
         Scene scene = new Scene(root);
 
         window.setScene(scene);
         window.show();
+    }
+
+    //Loads the current view again
+    public void refreshView() throws Exception{
+        changeView(currentView);
     }
 }
--- src/dk/aau/cs/ds308e/gui/controllers/SettingsController.java
+++ src/dk/aau/cs/ds308e/gui/controllers/SettingsController.java
@@ -1,12 +1,35 @@
 package dk.aau.cs.ds308e.gui.controllers;
 
 import dk.aau.cs.ds308e.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.util.Locale;
 
 public class SettingsController {
     @FXML
+    private ComboBox<Locale> languageSelector;
+
+    private ObservableList<Locale> languages = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        languages.addAll(new Locale("en","US"), new Locale("da","DA"));
+        languageSelector.setItems(languages);
+    }
+
+    @FXML
     private void backButtonAction(ActionEvent event) throws Exception{
         Main.gui.changeView("MainMenu");
+    }
+
+    @FXML
+    private void languageComboBoxAction(ActionEvent event) throws Exception{
+        Locale selectedLanguage = languageSelector.getSelectionModel().getSelectedItem();
+        Main.gui.setLanguage(selectedLanguage);
+        Main.gui.refreshView();
     }
 }
