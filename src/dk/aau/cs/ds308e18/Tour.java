95a1abbd3eccf0d74511fa023cc2c91d4695dbea
Ruofan Wu <rwu17@student.aau.dk>
2018-11-21 11:06:08 +0100

    generate tour file

--- src/dk/aau/cs/ds308e18/Tour.java
+++ src/dk/aau/cs/ds308e18/Tour.java
@@ -1,6 +1,12 @@
 package dk.aau.cs.ds308e18;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Tour {
 
@@ -14,6 +20,8 @@ public class Tour {
 
     String driver;
 
+    String directory = "resources/GenerateTour.csv";
+
     float tourDate;
 
     float packingDate;
@@ -21,9 +29,45 @@ public class Tour {
     float breakTime;
 
 
-    void generateTour(){
+    void CreateCSVFile(String tourName){
+        File csvFile = new File(tourName + ".csv");
+        try {
+            csvFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void generateTour(String directory) throws IOException {//Only generate one tour plan
+
+        FileWriter writer = new FileWriter(directory);
+
+        List<Order> orders = Arrays.asList(
+                new Order(),
+                new Order(),
+                new Order()
+        );
+
+        CSVUtils.writeLine(writer, Arrays.asList("", "", ""));
+
+        for (Order o : orders){
+            List<String> list = new ArrayList<>();
+            /*
+            list.add(o.);
+            list.add(o.);
+            list.add(o.);
+            */
+
+            CSVUtils.writeLine(writer, list);
+        }
+
+        writer.flush();
+        writer.close();
 
     }
 
+    void generateTourList(){//Generates a list of all tour plans
+
+    }
 
 }
