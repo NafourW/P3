431f33192873bb2e8bd4525dfdc7fafa54c2b45a
Ruofan Wu <rwu17@student.aau.dk>
2018-11-23 13:41:09 +0100

    OOP - order constructor

--- src/dk/aau/cs/ds308e18/Order.java
+++ src/dk/aau/cs/ds308e18/Order.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Order {
     ArrayList<Ware> wares;
 
+    /*
     String adress;
 
     String moreInfo;
@@ -21,8 +22,68 @@ public class Order {
     float pickupDate;
 
     float timeRequired;
+    */
 
+    long plukrute;
 
+    String order;
 
+    String returordreReference;
 
+    String ekspeditionsStatus;
+
+    String navn;
+
+    String modtagelsesDato;
+
+    String gadeNavn;
+
+    long postNummer;
+
+    long receipt;
+
+    boolean afhentning;
+
+    String sfhentningsDato;
+
+    int leveringsUge;
+
+    String lagerSted;
+
+    String ordreKategori;
+
+    String fleetOwner;
+
+    boolean udskrevet;
+
+    String rute;
+
+    boolean FV;
+
+    String projekt;
+
+    public Order(long plukrute, String order, String returordreReference, String ekspeditionsStatus, String navn,
+                 String modtagelsesDato, String gadeNavn, long postNummer, long receipt, boolean afhentning,
+                 String sfhentningsDato, int leveringsUge, String lagerSted, String ordreKategori, String fleetOwner,
+                 boolean udskrevet, String rute, boolean FV, String projekt) {
+        this.plukrute = plukrute;
+        this.order = order;
+        this.returordreReference = returordreReference;
+        this.ekspeditionsStatus = ekspeditionsStatus;
+        this.navn = navn;
+        this.modtagelsesDato = modtagelsesDato;
+        this.gadeNavn = gadeNavn;
+        this.postNummer = postNummer;
+        this.receipt = receipt;
+        this.afhentning = afhentning;
+        this.sfhentningsDato = sfhentningsDato;
+        this.leveringsUge = leveringsUge;
+        this.lagerSted = lagerSted;
+        this.ordreKategori = ordreKategori;
+        this.fleetOwner = fleetOwner;
+        this.udskrevet = udskrevet;
+        this.rute = rute;
+        this.FV = FV;
+        this.projekt = projekt;
+    }
 }
--- src/dk/aau/cs/ds308e18/ReadFile.java
+++ src/dk/aau/cs/ds308e18/ReadFile.java
@@ -1,14 +1,14 @@
 package dk.aau.cs.ds308e18;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadFile {//Class that reads CSV files
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        //String directory = scanner.nextLine().toString();
+        ArrayList<Order> OrderList = new ArrayList<>();
 
         String directory = "resources/ordrer_tilvalg.csv"; //Directory for the file with orders
 
@@ -18,20 +18,25 @@ public class ReadFile {//Class that reads CSV files
 
         ReadFile test = new ReadFile(); //test object
 
-        test.orderFile(directory); //read file with orders
+        test.orderFile(directory, OrderList); //read file with orders
+
+        System.out.print(OrderList);
+        /*
 
         test.orderItems(directory1); //read file with wares within an order
 
         System.out.println("\n\n");
 
         test.itemTypes(directory2); //read file with existing ware types
+        */
     }
 
     //convert xlsx file to csv file
     void xlsxToCSV(File inputFile, File outputFile){
     }
 
-    void orderFile(String directory){
+    ArrayList orderFile(String directory, ArrayList<Order> orderList){
+
         String line = ""; //this string will be filled with read line
 
         String cvsSplitBy = ";"; //split the read line when encountering ';' which is used in the CSV file to
@@ -47,7 +52,7 @@ public class ReadFile {//Class that reads CSV files
                 if (Order[0].contains("M")){//skips the first line, that only contains the names of categories
                     continue;
                 }
-
+                /*
                 //print out the array with splitted parts
                 System.out.println("Order [" +
                         "Plukrute: " + Order[1] +
@@ -70,10 +75,74 @@ public class ReadFile {//Class that reads CSV files
                         ", FV: " + Order[18] +
                         ", Projekt: " + Order[19] +
                         "]");
+                        */
+
+                long plukrute = Long.valueOf(Order[1]);
+
+                long postNummer;
+                if(Order[8].matches("[0-9]+") && Order[8].length() > 2)
+                {
+                    postNummer = Long.valueOf(Order[8]);
+                } else {
+                    postNummer = 0;
+                }
+
+                long receipt;
+
+                if (!(Order[9].isEmpty())){
+                    receipt = Long.valueOf(Order[9]);
+                } else {
+                    receipt = 0;
+                }
+
+                boolean afhentning;
+                if(Order[10].toLowerCase().equals("ja")){
+                    afhentning = true;
+                } else {
+                    afhentning = false;
+                }
+
+                String leveringsUgeString = Order[12].replace(",", ".");
+                float leveringsUgeFloat;
+                int leveringsUge = 0;
+
+                if(!(Order[12].isEmpty())){
+                    leveringsUgeFloat = Float.valueOf(leveringsUgeString);
+                    leveringsUge = (int) leveringsUgeFloat;
+                }
+
+                boolean udskrevet;
+                if (Order[16].toLowerCase().equals("ja")){
+                    udskrevet = true;
+                } else {
+                    udskrevet = false;
+                }
+
+                boolean FV;
+                if(Order[18].toLowerCase().equals("ja")){
+                    FV = true;
+                } else {
+                    FV = false;
+                }
+
+                //1 = long, done
+                //3 = long, done
+                //8 = long, done
+                //9 = long, done
+                //10 = boolean, done
+                //16 = boolean, done
+                //18 = boolean
+
+                Order order = new Order(plukrute, Order[2], Order[3], Order[4], Order[5], Order[6], Order[7],
+                        postNummer, receipt, afhentning, Order[11], leveringsUge, Order[13], Order[14], Order[15],
+                        udskrevet, Order[17], FV, Order[19]);
+
+                orderList.add(order);
             }
         } catch (IOException e){
             e.printStackTrace();
         }
+        return orderList;
     }
 
     void orderItems(String directory){//same function as "orderFile" method
