3e24a26d74855e25aed98a553105666c00eab820
GarridoY <33898925+GarridoY@users.noreply.github.com>
2018-11-27 10:37:42 +0100

    Revert "updated datamanagement"
    This reverts commit 825729383fdb5087918be32f8ae2c808878cccc1.

--- src/dk/aau/cs/ds308e18/io/DataManagement.java
+++ src/dk/aau/cs/ds308e18/io/DataManagement.java
@@ -156,49 +156,20 @@ public class DataManagement {
     Export everything from the order table.
     Print them in the terminal
     */
-    private ArrayList<Order> exportOrders() {
-        ArrayList<Order> orderList = new ArrayList<>();
-
-        long pluckroute;
-        int id;
-        String orderReference;
-        String expeditionStatus;
-        String customerName;
-        String date;
-        String address;
-        long zipCode;
-        long receipt;
-        boolean pickup;
-        String warehouse;
-        String category;
-        String fleetOwner;
-        boolean printed;
-        String route;
-        boolean FV;
-        String project;
-
+    private void exportOrders() {
         Connection conn = establishConnectionToDatabase();
         try {
             if (conn != null) {
                 Statement stmt = conn.createStatement();
                 ResultSet orders = stmt.executeQuery("SELECT * FROM orders");
-                ResultSet rsLength = stmt.executeQuery("SELECT COUNT(*) FROM orders");
-                int length = rsLength.getRow();
 
-                while(orders.next()) {
-                    for(int i = 1; i < length; i++) {
-                        System.out.println(orders.getString(i));
-                    }
+                while (orders.next()) {
+                    System.out.println(orders.getString(1));
                 }
             }
-
-
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        return orderList;
     }
 
     /*
@@ -225,6 +196,5 @@ public class DataManagement {
         DataManagement db = new DataManagement();
         db.databaseSetup();
         db.importOrders();
-        db.exportOrders();
     }
 }
