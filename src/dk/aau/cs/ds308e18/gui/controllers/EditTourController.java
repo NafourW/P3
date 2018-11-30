08b41b1c9a72953065a621161220b8d33cb7603f
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-11-30 12:43:13 +0100

    edit tour done button fixed

--- src/dk/aau/cs/ds308e18/gui/controllers/EditTourController.java
+++ src/dk/aau/cs/ds308e18/gui/controllers/EditTourController.java
@@ -1,10 +1,13 @@
 package dk.aau.cs.ds308e18.gui.controllers;
 
+import dk.aau.cs.ds308e18.Main;
 import dk.aau.cs.ds308e18.gui.ISelectionController;
 import dk.aau.cs.ds308e18.model.Tour;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
 
 public class EditTourController implements ISelectionController {
 
@@ -33,8 +36,8 @@ public class EditTourController implements ISelectionController {
     }
 
     @FXML
-    private void doneButtonAction(ActionEvent event) {
-
+    private void doneButtonAction(ActionEvent event) throws IOException {
+        Main.gui.changeView("TourList");
     }
 
     @Override
