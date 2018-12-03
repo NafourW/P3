cf568fb3b3df80b0c3c84b4fda2358031fb33197
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-12-03 13:29:16 +0100

    testen fejler ikke længere

--- src/dk/aau/cs/ds308e18/function/TourGenerator.java
+++ src/dk/aau/cs/ds308e18/function/TourGenerator.java
@@ -24,37 +24,35 @@ public class TourGenerator {
 
         //iterate through each order
         for (Order o : orders) {
-            //check if this is an unused date
-            if (!o.getDate().equals(date)) {
-                //use the order's date
-                date = o.getDate();
+            //use the order's date
+            date = o.getDate();
 
-                boolean dateAlreadyUsed = false;
-                boolean orderLimitExceeded = false;
+            boolean dateAlreadyUsed = false;
+            boolean orderLimitExceeded = false;
 
-                //iterate through each tour
-                for (Tour t : generatedTours) {
-                    //check if this date was already used by a previous tour
-                    if (date.equals(t.getTourDate()))
-                        dateAlreadyUsed = true;
+            //iterate through each tour
+            for (Tour t : generatedTours) {
+                //check if this date was already used by a previous tour
+                if (date.equals(t.getTourDate()))
+                    dateAlreadyUsed = true;
 
-                    //check if tour has reached the max orders limit
-                    if (t.getOrders().size() >= MAX_ORDERS_PER_TOUR)
-                        orderLimitExceeded = true;
+                //check if tour has reached the max orders limit
+                if (t.getOrders().size() >= MAX_ORDERS_PER_TOUR)
+                    orderLimitExceeded = true;
 
-                    //if the date is used by another tour, and that tour has not reached the order limit,
-                    //use the previous tour
-                    if (dateAlreadyUsed && !orderLimitExceeded)
-                        tour = t;
-                }
-
-                //if no tour has used this date (or max orders has been reached)
-                //make a new tour
-                if (!dateAlreadyUsed || orderLimitExceeded) {
-                    tour = new Tour(date);
-                    generatedTours.add(tour);
-                }
+                //if the date is used by another tour, and that tour has not reached the order limit,
+                //use the previous tour
+                if (dateAlreadyUsed && !orderLimitExceeded)
+                    tour = t;
             }
+
+            //if no tour has used this date (or max orders has been reached)
+            //make a new tour
+            if (!dateAlreadyUsed || orderLimitExceeded) {
+                tour = new Tour(date);
+                generatedTours.add(tour);
+            }
+
             //add the order to the tour
             tour.addOrder(o);
         }
