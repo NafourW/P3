a20eab7eb7bb8aca28f1c4d4b980a4c416b5ba42
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-12-11 10:38:35 +0100

    exportfile forhåbentlig ikke ødelagt

--- src/dk/aau/cs/ds308e18/io/ExportFile.java
+++ src/dk/aau/cs/ds308e18/io/ExportFile.java
@@ -41,32 +41,34 @@ public class ExportFile {
          *
          */
 
-            String orderExportPath = path + "/" + tour.getTourID() + ".csv"; //create a file for these orders
+        String orderExportPath = path + "/" + tour.getTourID() + ".csv"; //create a file for these orders
 
-            FileWriter writer1 = new FileWriter(orderExportPath); //ready to append the file
+        FileWriter writer = new FileWriter(orderExportPath); //ready to append the file
+        FileWriter writer1 = new FileWriter(orderExportPath); //ready to append the file
 
-            //write first row of the table (attritubes)
-            CSVUtils.writeLine(writer1, Arrays.asList("M", "Plukrute", "Ordre", "Returordre reference",
-                    "Ekspeditionsstatus", "Leveringsnavn", "Ønsket modtagelsesdato", "Følgeseddel", "Plukrute2",
-                    "Gadenavn", "Postnummer", "Følgeseddel3", "Afhentning", "Afhentningsdato", "Leverungs Uge",
-                    "Pakkedato", "Lagersted", "Ordrekategori", "Flådeejer", "Udskrevet", "Projekt"));
+        //write first row of the table (attritubes)
+        CSVUtils.writeLine(writer1, Arrays.asList("M", "Plukrute", "Ordre", "Returordre reference",
+                "Ekspeditionsstatus", "Leveringsnavn", "Ønsket modtagelsesdato", "Følgeseddel", "Plukrute2",
+                "Gadenavn", "Postnummer", "Følgeseddel3", "Afhentning", "Afhentningsdato", "Leverungs Uge",
+                "Pakkedato", "Lagersted", "Ordrekategori", "Flådeejer", "Udskrevet", "Projekt"));
 
-            //go through each order within the tour
-            for (Order order: tour.getOrders()){
+        //go through each order within the tour
+        for (Order order: tour.getOrders()){
 
-                //write the information of the order in the list of orders
-                CSVUtils.writeLine(writer, Arrays.asList(String.valueOf(order.getPluckRoute()), order.getID(),
-                        order.getOrderReference(), order.getExpeditionStatus(), order.getCustomerName(),
-                        order.getDate().toString(), String.valueOf(order.getReceipt()), String.valueOf(order.getPluckRoute()),
-                        order.getAddress(), String.valueOf(order.getZipCode()), String.valueOf(order.getReceipt()),
-                        order.getCategory(), order.getDate().toString(), String.valueOf(order.getWeekNumber()),
-                        order.getDate().toString(), order.getWarehouse(), order.getCategory(),
-                        order.getFleetOwner(), String.valueOf(order.isPrinted()), order.getProject()));
-            }
+            //write the information of the order in the list of orders
+            CSVUtils.writeLine(writer, Arrays.asList(String.valueOf(order.getPluckRoute()), order.getID(),
+                    order.getOrderReference(), order.getExpeditionStatus(), order.getCustomerName(),
+                    order.getDate().toString(), String.valueOf(order.getReceipt()), String.valueOf(order.getPluckRoute()),
+                    order.getAddress(), String.valueOf(order.getZipCode()), String.valueOf(order.getReceipt()),
+                    order.getCategory(), order.getDate().toString(), String.valueOf(order.getWeekNumber()),
+                    order.getDate().toString(), order.getWarehouse(), order.getCategory(),
+                    order.getFleetOwner(), String.valueOf(order.isPrinted()), order.getProject()));
+
             //ending appending to the file of list of orders
             writer1.flush();
             writer1.close();
         }
+
         //ending appending to the file of list of tours
         writer.flush();
         writer.close();
