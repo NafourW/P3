8e308d78871bf0d442b01cf23ae3445b5fe44baa
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-12-19 20:15:53 +0100

    comment til tourgenerator settings

--- src/dk/aau/cs/ds308e18/function/tourgen/TourGeneratorSettings.java
+++ src/dk/aau/cs/ds308e18/function/tourgen/TourGeneratorSettings.java
@@ -1,5 +1,10 @@
 package dk.aau.cs.ds308e18.function.tourgen;
 
+/*
+This class is used to store the chosen settings
+for tour generation. It is passed to the tour generator
+from GeneratorProgressController, which gets it from TourGeneratorController
+*/
 public class TourGeneratorSettings {
     public enum planningMethod{
         leastTime,
